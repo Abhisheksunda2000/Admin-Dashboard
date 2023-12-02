3fac106624823f18c934f7583476f08f3b14e808
Abhishek Choudhary <abhishek.choudhary@cogoport.com>
2023-12-03 05:04:30 +0530

    fetch data from api done

--- src/pages/dashboard/Dashboard.jsx
+++ src/pages/dashboard/Dashboard.jsx
@@ -1,12 +1,37 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react';
 import Dashbar from '../../components/dashbar/Dashbar';
-import entries from '../../Data';
-import "./dashboard.css";
+import fetchData from '../../Data'; // Import fetchData function
+import "./dashboard.css"
 
 export default function Dashboard() {
+  
+  const [data, setData] = useState([]);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    const fetchDataAsync = async () => {
+      try {
+        const result = await fetchData();
+        setData(result);
+        setLoading(false); // Set loading to false when data is fetched
+      } catch (error) {
+        console.error('Error fetching data:', error);
+        setLoading(false); // Set loading to false on error too
+      }
+    };
+
+    fetchDataAsync();
+  }, []);
+
+  if (loading) {
+    return <div>Loading...</div>;
+  }
+
   return (
-    <>   
-         <Dashbar data = {entries}/>
-    </>
-  )
+    <div>
+      <Dashbar data={data} />
+    </div>
+  );
 }
+
+
